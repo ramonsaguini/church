2d33a86132780f7cc752b8cfa10dfdbad23eb056
Ramon Andrade <124772095+ramonsaguini@users.noreply.github.com>
2024-03-22 18:24:48 +0000

    upp5

--- src/Components/Maceio/Mapa/index.jsx
+++ src/Components/Maceio/Mapa/index.jsx
@@ -8,7 +8,7 @@ const Mapa = () => {
         <img className='img-mapa' src="../../../../public/img/Maceio/mapa.png" alt="mapa" />
       </a>
       <div className="map-text" onClick={() => window.open('https://www.google.com/maps?q=MANAH+CHURCH+-+Av+Men+Marcelo,+Antares,+Maceió+-+AL,+57048-015,+Brasil', '_blank')}>
-        <h2> <img src="../../../../../public/img/google-maps.png" width={30} alt="" /> Endereço <img width={50} src="../../../../../public/img/house.png" alt="" /> </h2>
+        <h2> <img src="../img/google-maps.png" width={30} alt="" /> Endereço <img width={50} src="../img/house.png" alt="" /> </h2>
         <p>Av. Men Marcelo - Antares, Maceió - AL, 57048-015, Brasil</p>
       </div>
     </div>
