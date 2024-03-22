5d779132f72cc24d357846579946fb16eb870d94
Ramon Andrade <124772095+ramonsaguini@users.noreply.github.com>
2024-03-22 13:00:24 +0000

    up8

--- src/Components/Main/index.jsx
+++ src/Components/Main/index.jsx
@@ -11,19 +11,19 @@ function Main() {
       <div className="d-flex justify-content-center align-items-center m-3">
         <div className='text-center text-decoration-none'>
           <Link to="/Manchester" className="text-decoration-none">
-            <img src="./public/img/manah-logo-black.jpg" alt="manchester" className="circle-image img-locals" />
+            <img src="public/img/manah-logo-black.jpg" alt="manchester" className="circle-image img-locals" />
             <p className='black'>Manchester </p>
           </Link>
         </div>
         <div className='text-center text-decoration-none'>
           <Link to="/Liverpool" className="text-decoration-none">
-            <img src="../public/img/manah-logo-black.jpg" alt="liverpool" className="circle-image img-locals" />
+            <img src="public/img/manah-logo-black.jpg" alt="liverpool" className="circle-image img-locals" />
             <p className='black'>Liverpool</p>
           </Link>
         </div>
         <div className='text-center text-decoration-none'>
           <Link to="/Maceio" className="text-decoration-none">
-            <img src="/public/img/manah-logo-black.jpg" alt="maceio" className="circle-image img-locals" />
+            <img src="public/img/manah-logo-black.jpg" alt="maceio" className="circle-image img-locals" />
             <p className='black'>Maceio</p>
           </Link>
         </div>
